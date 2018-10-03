074486cb978af83c87802ce6a21b3ccf10a7da30
stepforwardtech <jrstrayhorn@gmail.com>
2018-10-03 07:54:48 -0400

    updating domain model

--- Models/Feature.cs
+++ Models/Feature.cs
@@ -2,7 +2,7 @@ namespace vega.Models
 {
     public class Feature
     {
-        public int FeatureId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
--- Models/MakeModel.cs
+++ Models/MakeModel.cs
@@ -2,7 +2,7 @@ namespace vega.Models
 {
     public class MakeModel
     {
-        public int MakeModelId { get; set; }
+        public int Id { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
     }
