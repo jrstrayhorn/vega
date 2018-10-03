d6197bfe9be0b958a2626aac9bfc3fb4457d11e5
stepforwardtech <jrstrayhorn@gmail.com>
2018-10-03 07:54:02 -0400

    wip - creating domain model for feature, make apis

---
+++ Models/Feature.cs
@@ -0,0 +1,8 @@
+namespace vega.Models
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/MakeModel.cs
@@ -0,0 +1,9 @@
+namespace vega.Models
+{
+    public class MakeModel
+    {
+        public int MakeModelId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
