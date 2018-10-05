2d1af8869273911ea05622cca2da01251736c2e1
stepforwardtech <jrstrayhorn@gmail.com>
2018-10-05 07:44:59 -0400

    populating features table

---
+++ Migrations/20181005113425_PopulateFeatures.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class PopulateFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SET IDENTITY_INSERT Features ON");
+            migrationBuilder.Sql("INSERT Features (Id, Name) VALUES (1, 'ABS Brakes')");
+            migrationBuilder.Sql("INSERT Features (Id, Name) VALUES (2, 'Airbags')");
+            migrationBuilder.Sql("INSERT Features (Id, Name) VALUES (3, 'Bottle Holder')");
+            migrationBuilder.Sql("SET IDENTITY_INSERT Features OFF");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
