284b0f01afd68b3b828845071c70734ed665b76d
stepforwardtech <jrstrayhorn@gmail.com>
2018-10-09 07:43:53 -0400

    populating MakeModels in db

---
+++ Migrations/20181009113749_PopulateMakeModels.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class PopulateMakeModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SET IDENTITY_INSERT MakeModels ON");
+            migrationBuilder.Sql("INSERT MakeModels (Id, Make, Model) VALUES (1, 'BMW', 'E90')");
+            migrationBuilder.Sql("INSERT MakeModels (Id, Make, Model) VALUES (2, 'Hyundai', 'Sonata')");
+            migrationBuilder.Sql("INSERT MakeModels (Id, Make, Model) VALUES (3, 'Nissan', 'Altima')");
+            migrationBuilder.Sql("SET IDENTITY_INSERT MakeModels OFF");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
