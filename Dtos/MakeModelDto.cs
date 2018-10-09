b5293e101c965b7a4d6bd9babbc20a2400b785e6
stepforwardtech <jrstrayhorn@gmail.com>
2018-10-09 07:45:51 -0400

    adding makemodel dto, mapping profile

---
+++ Dtos/MakeModelDto.cs
@@ -0,0 +1,9 @@
+namespace vega.Dtos
+{
+    public class MakeModelDto
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
