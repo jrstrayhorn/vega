aa77e741b8c4433e2a34d22f2f0a5413203664ea
stepforwardtech <jrstrayhorn@gmail.com>
2018-10-09 07:34:48 -0400

    wip - created features api endpoint

---
+++ Controllers/FeaturesController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using vega.Dtos;
+using vega.Models;
+
+namespace vega.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
+
+        public FeaturesController(VegaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<List<FeatureDto>> GetAll()
+        {
+            return _mapper.Map<List<FeatureDto>>(_context.Features.ToList());
+        }
+    }
+}
